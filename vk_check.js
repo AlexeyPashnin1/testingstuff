4d97d43f11de3a94ff9ad855fe50afa6c53dcf46
Alexey <109278274+AlexeyPashnin1@users.noreply.github.com>
2023-04-08 21:41:31 +0500

    Update vk_check.js

--- vk_check.js
+++ vk_check.js
@@ -1,5 +1,3 @@
-// @version      0.5
-
 function checkIfNoGo() {
     const timerID = window.setTimeout(checkIfNoGo, 1500);
     let arrDialogs = ['291479035'];
@@ -11,20 +9,5 @@ function checkIfNoGo() {
             spanText.style.margin = '0 0 0 5px';
             document.querySelector('.im-page--title').appendChild(spanText);
         } else {}
-    //}, 1000);
-    // if (arrDialogs.includes(location.search.replace('?sel=', ''))) {
-    //     document.querySelectorAll('.nim-dialog').forEach((el) => {
-    //         (el).addEventListener("click", () => {
-    //             setTimeout(() => {
-    //                 let spanText = document.createElement("span");
-    //                 spanText.className = 'test';
-    //                 spanText.textContent = 'не было';
-    //                 spanText.style
-    //                 spanText.style.margin = '0 0 0 5px';
-    //                 document.querySelector('.im-page--title').appendChild(spanText);
-    //             }, 1000);
-    //         });
-    //     });
-    // }
 }
 checkIfNoGo();
